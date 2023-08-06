a485d90fc2b17608741996d75270b8b804366860
alanmoraales <alanmoraales845@gmail.com>
2023-08-06 17:41:34 -0600

    update PageLimitContainer

--- src/components/atoms/PageLimitContainer/PageLimitContainer.tsx
+++ src/components/atoms/PageLimitContainer/PageLimitContainer.tsx
@@ -3,18 +3,35 @@ import { css } from "@styled/css";
 
 interface IPageLimitContainerProps {
   children: ReactNode;
+  isHalfWidth?: boolean;
+  isBlogPost?: boolean;
 }
 
-const PageLimitContainer = ({ children }: IPageLimitContainerProps) => (
-  <div
-    className={css({
-      width: "90%",
-      margin: "0 auto",
-      maxWidth: "1024px",
-    })}
-  >
-    {children}
-  </div>
-);
+const PageLimitContainer = ({
+  children,
+  isHalfWidth = false,
+  isBlogPost = false,
+}: IPageLimitContainerProps) => {
+  const isHalfWidthStyle = isHalfWidth
+    ? {
+        display: { base: "block", lg: "grid" },
+        gridTemplateColumns: { base: "unset", lg: "65% auto" },
+      }
+    : {};
+  const maxWidth = isBlogPost ? "60ch" : "1024px";
+
+  return (
+    <div
+      className={css({
+        width: "90%",
+        margin: "0 auto",
+        maxWidth,
+        ...isHalfWidthStyle,
+      })}
+    >
+      {children}
+    </div>
+  );
+};
 
 export default PageLimitContainer;
