04e48083b0a9de3aca03cc23116e3090f7a45fea
alanmoraales <alanmoraales845@gmail.com>
2023-08-06 17:43:07 -0600

    simplify code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,15 @@
-import Link from "next/link";
-import Heading from "@atoms/Heading";
-import PageLimitContainer from "@atoms/PageLimitContainer";
 import Navbar from "@organisms/Navbar";
 import notionService from "@services/notion";
+import RecentPostsSection from "@organisms/RecentPostsSection/RecentPostsSection";
 
 const Home = async () => {
   const posts = await notionService.getPosts();
   return (
     <>
       <Navbar />
-      <PageLimitContainer>
-        <main>
-          <Heading>Posts</Heading>
-          {posts.map((post) => (
-            <Link href={`/posts/${post.slug}`} key={post.slug}>
-              <h2>{post.title}</h2>
-              <p>{post.summary}</p>
-            </Link>
-          ))}
-        </main>
-      </PageLimitContainer>
+      <main>
+        <RecentPostsSection posts={posts} />
+      </main>
     </>
   );
 };
