4364665fd9ca035555dbc9353472292349094ac9
alanmoraales <alanmoraales845@gmail.com>
2023-08-06 12:35:44 -0600

    render Navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,26 @@
-import notionService from "@/services/notion";
 import Link from "next/link";
+import Heading from "@atoms/Heading";
+import PageLimitContainer from "@atoms/PageLimitContainer";
+import Navbar from "@organisms/Navbar";
+import notionService from "@services/notion";
 
 const Home = async () => {
   const posts = await notionService.getPosts();
   return (
-    <main>
-      <h4>Posts</h4>
-      {posts.map((post) => (
-        <Link href={`/posts/${post.slug}`} key={post.slug}>
-          <h2>{post.title}</h2>
-          <p>{post.summary}</p>
-        </Link>
-      ))}
-    </main>
+    <>
+      <Navbar />
+      <PageLimitContainer>
+        <main>
+          <Heading>Posts</Heading>
+          {posts.map((post) => (
+            <Link href={`/posts/${post.slug}`} key={post.slug}>
+              <h2>{post.title}</h2>
+              <p>{post.summary}</p>
+            </Link>
+          ))}
+        </main>
+      </PageLimitContainer>
+    </>
   );
 };
 
