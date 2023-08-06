8d02a9ab325a139e20c4bb7f3f5d0298e871c6e9
alanmoraales <alanmoraales845@gmail.com>
2023-08-06 17:42:27 -0600

    add RecentPostsSection

---
+++ src/components/organisms/RecentPostsSection/RecentPostsSection.tsx
@@ -0,0 +1,39 @@
+import PostCard from "@molecules/PostCard";
+import PreviewSectionTemplate from "@templates/PreviewSectionTemplate";
+import { format } from "date-fns";
+import { es } from "date-fns/locale";
+import type { IPost } from "@services/notion";
+import Flex from "@atoms/Flex";
+import { Fragment } from "react";
+import When from "@atoms/When/When";
+import Divider from "@atoms/Divider/Divider";
+
+interface IRecentPostsSectionProps {
+  posts: IPost[];
+}
+
+const RecentPostsSection = ({ posts }: IRecentPostsSectionProps) => (
+  <PreviewSectionTemplate
+    title="Lecturas recientes"
+    goToHref="/posts"
+    isHalfWidth
+  >
+    <Flex flexDirection="column" gap="lg">
+      {posts.slice(0, 2).map((post, index) => (
+        <Fragment key={post.slug}>
+          <PostCard
+            {...post}
+            href={`/posts/${post.slug}`}
+            key={post.slug}
+            publishedAt={format(post.publishedAt, "PP", { locale: es })}
+          />
+          <When condition={index + 1 !== posts.length}>
+            <Divider />
+          </When>
+        </Fragment>
+      ))}
+    </Flex>
+  </PreviewSectionTemplate>
+);
+
+export default RecentPostsSection;
