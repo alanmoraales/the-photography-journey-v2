23c5e6324fcb36e7ce5683f6655017e766f4817b
alanmoraales <alanmoraales845@gmail.com>
2023-08-06 17:41:05 -0600

    update NavbarLink color

--- src/components/atoms/Navbar/NavbarLink.tsx
+++ src/components/atoms/Navbar/NavbarLink.tsx
@@ -11,7 +11,7 @@ const NavbarLink = ({ children, href }: INavbarLinkProps) => (
   <Link href={href}>
     <Heading
       level="h6"
-      color="gray"
+      color="light"
       textDecoration="underline"
       _hover={{
         color: "primary",
