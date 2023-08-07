1f7a78c0fd96c752298ce62dc452a9630f41f7e2
alanmoraales <alanmoraales845@gmail.com>
2023-08-07 00:54:51 -0600

    fix spacing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,17 @@
 import Navbar from "@organisms/Navbar";
-import notionService from "@services/notion";
+import Flex from "@atoms/Flex";
 import RecentPostsSection from "@organisms/RecentPostsSection/RecentPostsSection";
+import notionService from "@services/notion";
 
 const Home = async () => {
   const posts = await notionService.getPosts();
   return (
-    <>
+    <Flex flexDirection="column" gap="xl" paddingY="xl">
       <Navbar />
       <main>
         <RecentPostsSection posts={posts} />
       </main>
-    </>
+    </Flex>
   );
 };
 
