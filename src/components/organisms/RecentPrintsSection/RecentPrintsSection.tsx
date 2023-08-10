657c2caca718a454e622979f2a230c2fa8f86b5b
alanmoraales <alanmoraales845@gmail.com>
2023-08-09 18:59:51 -0600

    add RecentPrintsSection

---
+++ src/components/organisms/RecentPrintsSection/RecentPrintsSection.tsx
@@ -0,0 +1,19 @@
+import PrintsCarousel from "@organisms/PrintsCarousel";
+import { IPrint } from "@services/notion";
+import PreviewSectionTemplate from "@templates/PreviewSectionTemplate";
+
+interface IRecentPrintsSectionProps {
+  prints: IPrint[];
+}
+
+const RecentPrintsSection = ({ prints }: IRecentPrintsSectionProps) => (
+  <PreviewSectionTemplate
+    title="Más fotos impresas"
+    goToHref="/prints"
+    goToLabel="Ver todas"
+  >
+    <PrintsCarousel prints={prints} />
+  </PreviewSectionTemplate>
+);
+
+export default RecentPrintsSection;
